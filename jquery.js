feaea880e69dd8384d759bb6fe8a67774f93b4d1
juhee0202 <Juhee@Jus-MacBook-Pro.local>
2015-01-18 00:01:46 +0800

    first

---
+++ jquery.js
@@ -0,0 +1,18 @@
+ //from open source http://jsfiddle.net/YtJcL/
+
+ $(document).ready(function(e) {   
+ 
+ $(".scroll").click(function(event){
+         event.preventDefault();
+         //calculate destination place
+         var dest=0;
+         if($(this.hash).offset().top > $(document).height()-$(window).height()){
+              dest=$(document).height()-$(window).height();
+         }else{
+              dest=$(this.hash).offset().top;
+         }
+         //go to destination
+         $('html,body').animate({scrollTop:dest}, 1000,'swing');
+     });
+
+});
